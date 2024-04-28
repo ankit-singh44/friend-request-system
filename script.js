75b52a0722ee6e35e30756f46c05c70a8490cd88
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2024-04-28 15:44:13 +0530

    Send Friend request system

---
+++ script.js
@@ -0,0 +1,19 @@
+var rel=document.querySelector("h5");
+var add=document.querySelector("#add");
+
+var val=0;
+
+add.addEventListener("click",function(){
+    if(val==0){
+        rel.innerHTML="Friends";
+        rel.style.color="green";
+        add.innerHTML="Remove Friend";
+        val=1;
+    }
+    else{
+        rel.innerHTML="Stranger";
+        rel.style.color="red";
+        add.innerHTML="Add Friend";
+        val=0;
+    }
+})
