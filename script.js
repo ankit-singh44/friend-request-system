1ea9bbcca69c2ed8e12a9fcaeb507485c3dcae84
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2024-04-29 12:38:24 +0530

    error fix

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ var rel=document.querySelector("h5");
 var add=document.querySelector("#add");
 
 var val=0;
-
 add.addEventListener("click",function(){
     if(val==0){
         rel.innerHTML="Friends";
         rel.style.color="green";
         add.innerHTML="Remove Friend";
-        val=1;
+        val=1;  
     }
     else{
         rel.innerHTML="Stranger";
